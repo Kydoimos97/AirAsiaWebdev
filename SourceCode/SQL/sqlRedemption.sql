0f0aa163517477389cbb2eed10eafc989ae31e6f
Kydoimos97 <willemvanderschans97@gmail.com>
2022-12-04 20:48:00 -0700

    Changed Increment and added quantity log

--- SourceCode/SQL/sqlRedemption.sql
+++ SourceCode/SQL/sqlRedemption.sql
@@ -13,7 +13,8 @@ CREATE TABLE `redemption`
     `timestamp`    varchar(55),
     `pointsRedeemed`    varchar(255) NOT NULL,
     `accountId`    varchar(55)  NOT NULL,
-    `cardId`   varchar(55)  NOT NULL
+    `cardId`   varchar(250)  NOT NULL,
+    `quantity` varchar(250) NOT NULL
 
 ) ENGINE = InnoDB
   DEFAULT CHARSET = latin1;
@@ -33,7 +34,7 @@ ALTER TABLE `redemption`
 --
 ALTER TABLE `redemption`
     MODIFY `redeemId` int(8) NOT NULL AUTO_INCREMENT,
-    AUTO_INCREMENT = 2;
+    AUTO_INCREMENT = 1;
 
 --
 -- Dumping data for table `redemption`
